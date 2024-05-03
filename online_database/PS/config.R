2ffcbc84bc6a32a56717906d052474b7d9c661e3
DENG lab <36807212+dengxinb2315@users.noreply.github.com>
2024-05-03 10:04:40 +0800

    Update config.R

--- online_database/PS/config.R
+++ online_database/PS/config.R
@@ -1,1 +1,17 @@
+title = 'P. syringae TF Network'
+color = "blue"
 
+intro_path = "www/markdown/home.md"
+
+con_path = "www/markdown/contact.md"
+
+###
+Chip_data_path = "www/data/ChIP_data_PS_TF.csv"
+Chip_source = "Source"
+Chip_target = "Target"
+CHip_output_name = "ChIP_data_PS_TF.csv"
+
+SELEX_data_path = "www/data/SELEX_data_PS_TF.csv"
+SELEX_source = "Source"
+SELEX_target = "Target"
+SELEX_output_name = "SELEX_data_PS_TF.csv"

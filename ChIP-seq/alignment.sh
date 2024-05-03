7b3b063b6e0dc92feefa9aa169c05d164d7d8be0
DENG lab <36807212+dengxinb2315@users.noreply.github.com>
2024-05-03 09:31:28 +0800

    Update alignment.sh

--- ChIP-seq/alignment.sh
+++ ChIP-seq/alignment.sh
@@ -1,1 +1,5 @@
+#Build Reference
+bowtie2-build -f reference PS
 
+#alignment
+bowtie2 -p 20 -x ../reference/PS -1 ../02cleandata/R1_val_1.fq.gz -2 ../02cleandata/R2_val_2.fq.gz -S sample.sam 2>sample.align.log

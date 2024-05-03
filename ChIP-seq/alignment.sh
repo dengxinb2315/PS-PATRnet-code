dee59a84ab186fd77158cf5630f480d9c2bed307
DENG lab <36807212+dengxinb2315@users.noreply.github.com>
2024-05-03 09:34:27 +0800

    Update alignment.sh

--- ChIP-seq/alignment.sh
+++ ChIP-seq/alignment.sh
@@ -2,4 +2,13 @@
 bowtie2-build -f reference PS
 
 #alignment
-bowtie2 -p 20 -x ../reference/PS -1 ../02cleandata/R1_val_1.fq.gz -2 ../02cleandata/R2_val_2.fq.gz -S sample.sam 2>sample.align.log
+bowtie2 -p 20 -x ../reference/PS -1 ../02cleandata/R1_val_1.fq.gz -2 ../02cleandata/R2_val_2.fq.gz -S $sample.sam 2>$sample.align.log
+
+#sam to bam
+samtools sort $sample.unique.sam -@ 20 -o ./$sample.bam
+
+#merge two files
+samtools merge $sample.merge.bam $sample-1.bam $sample-2.bam
+
+#remove duplicates
+sambamba markdup --overflow-list-size 5928787 --tmpdir ./ -r ./$sample.merge.bam ./$sample.rmdup.bam -t 10

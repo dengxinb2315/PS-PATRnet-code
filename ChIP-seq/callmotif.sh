f4534bce1e4c46a3a0ef2346715cd8cdd55261c0
DENG lab <36807212+dengxinb2315@users.noreply.github.com>
2024-05-03 09:41:37 +0800

    Update callmotif.sh

--- ChIP-seq/callmotif.sh
+++ ChIP-seq/callmotif.sh
@@ -1,1 +1,6 @@
+#get fasta file from macs3
+cat $sample_summits.bed |awk '{print $1 "\t" $2-50 "\t" $3+50}' > $sample.bed
+bedtools getfasta -fi reference/PS.fna -bed $sample.bed -fo $sample.fasta
 
+#call motif
+meme-chip $sample.fasta -maxw 20 -oc $sample
